8377ac50358932cbc5053ea82a0cbf2f5044ef85
AClon314 <30747832+AClon314@users.noreply.github.com>
2026-06-14 16:49:07 +0800

    update v4.0.22 to v6.0.0; migrate to recipe.yaml (rattler-build)

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,25 +2,25 @@
 set -ex
 
 export EM_BINARYEN_ROOT=$PREFIX
+export EM_CONFIG=$PREFIX/lib/emscripten-$PKG_VERSION/.emscripten
 
 python tools/install.py $PREFIX/lib/emscripten-$PKG_VERSION/
 # remove leftovers
-rm $PREFIX/lib/emscripten-$PKG_VERSION/build_env_setup.sh
-rm $PREFIX/lib/emscripten-$PKG_VERSION/conda_build.sh
+rm -f $PREFIX/lib/emscripten-$PKG_VERSION/build_env_setup.sh
+rm -f $PREFIX/lib/emscripten-$PKG_VERSION/conda_build.sh
 
 python $RECIPE_DIR/link_bin.py
 
 # make emcc etc. executable
 chmod -R +x $PREFIX/lib/emscripten-$PKG_VERSION
 
-emcc --generate-config
+$PREFIX/bin/emcc --generate-config
 
 python $RECIPE_DIR/fix_emscripten_config.py
 
 pushd $PREFIX/lib/emscripten-$PKG_VERSION/
 echo "Checking node"
 file $(which node)
-npm install
 popd
 
 rm -rf $PREFIX/lib/emscripten-$PKG_VERSION/tests
@@ -29,7 +29,7 @@ rm -rf $PREFIX/lib/emscripten-$PKG_VERSION/tests
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" || "$CROSSCOMPILING_EMULATOR" != "" ]]; then
   export NODE_JS=$BUILD_PREFIX/bin/node
   echo "int main() {};" > asd.c
-  emcc asd.c
+  $PREFIX/bin/emcc asd.c
 fi
 
 # We should probably not do this
